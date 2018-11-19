eb52b1274673ed2561768c748ec098e6e0ce4776
Ken Huh <ken123777@gmail.com>
2018-11-19 13:31:22 +0900

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,9 +4,3 @@ import 'styles/index.less';
 // ================================
 // START YOUR APP HERE
 // ================================
-
-
-
-/* DO NOT REMOVE */
-module.hot.accept();
-/* DO NOT REMOVE */
